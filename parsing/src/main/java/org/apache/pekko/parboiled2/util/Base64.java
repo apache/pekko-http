ccd59fd804afc84926e772b150302213b6e2bf4d
Matthew de Detrich <matthew.dedetrich@aiven.io>
2023-02-20 23:23:51 +0100

    Add Apache Header

--- parsing/src/main/java/org/apache/pekko/parboiled2/util/Base64.java
+++ parsing/src/main/java/org/apache/pekko/parboiled2/util/Base64.java
@@ -1,3 +1,12 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
 /**
  * A very fast and memory efficient class to encode and decode to and from BASE64 in full accordance
  * with RFC 2045.<br><br>
