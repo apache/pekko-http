905fc9cf9de922f5de003cc9ac9d1e3dd22e710f
Matthew de Detrich <matthew.dedetrich@aiven.io>
2023-02-20 23:23:51 +0100

    Add Apache Header

--- http-core/src/main/scala/org/apache/pekko/http/impl/engine/http2/util/AsciiTreeLayout.scala
+++ http-core/src/main/scala/org/apache/pekko/http/impl/engine/http2/util/AsciiTreeLayout.scala
@@ -1,3 +1,12 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * license agreements; and to You under the Apache License, version 2.0:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * This file is part of the Apache Pekko project, derived from Akka.
+ */
+
 /* sbt -- Simple Build Tool
  * Copyright 2011 Mark Harrah, Eugene Yokota
  *
