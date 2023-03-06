5f45baecc813510f444bcfd9fe44326ef335810a
Johannes Rudolph <johannes.rudolph@gmail.com>
2023-03-06 13:00:52 +0100

    run full validation when github workflows change (#111)

--- project/ValidatePullRequest.scala
+++ project/ValidatePullRequest.scala
@@ -111,7 +111,7 @@ object ValidatePullRequest extends AutoPlugin {
   val additionalTasks = settingKey[Seq[TaskKey[_]]]("Additional tasks for pull request validation")
 
   // The set of (top-level) files or directories to watch for build changes.
-  val BuildFilesAndDirectories = Set("project", "build.sbt", ".github/workflows")
+  val BuildFilesAndDirectories = Set("project", "build.sbt", ".github")
 
   def changedDirectoryIsDependency(changedDirs: Set[String], name: String,
       graphsToTest: Seq[(Configuration, ModuleGraph)])(log: Logger): Boolean = {
