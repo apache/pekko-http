ab3e233c428bf36ffe38ec85d6545a81eb4e3422
Matthew de Detrich <matthew.dedetrich@aiven.io>
2023-03-20 15:06:24 +0100

    Update Scala 2.12 version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,7 +34,7 @@ object Dependencies {
 
   val scalafixVersion = _root_.scalafix.sbt.BuildInfo.scalafixVersion // grab from plugin
 
-  val scala212Version = "2.12.15"
+  val scala212Version = "2.12.17"
   val scala213Version = "2.13.10"
   val allScalaVersions = Seq(scala213Version, scala212Version)
 
