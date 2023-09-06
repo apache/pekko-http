9c3dc3778ed65103465d40c3dbe69c89729c6580
kerr <hepin.p@alibaba-inc.com>
2023-09-07 00:34:40 +0800

    =sbt Update to Scala 3.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
 
   val scala212Version = "2.12.18"
   val scala213Version = "2.13.12"
-  val scala3Version = "3.3.0"
+  val scala3Version = "3.3.1"
   val allScalaVersions = Seq(scala213Version, scala212Version, scala3Version)
 
   val Versions = Seq(
