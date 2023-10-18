2c64ef510ef2722314ae82a8a35b230eeb64c12d
PJ Fanning <pjfanning@users.noreply.github.com>
2023-10-18 16:15:30 +0100

    allow test with pekko 1.0.x snapshot

--- project/PekkoDependency.scala
+++ project/PekkoDependency.scala
@@ -44,6 +44,7 @@ object PekkoDependency {
       case None =>
         Option(System.getProperty("pekko.http.build.pekko.version")) match {
           case Some("main")           => mainSnapshot
+          case Some("snapshot-1.0.x") => snapshot10x
           case Some("default") | None => Artifact(defaultVersion)
           case Some(other)            => Artifact(other, true)
         }
@@ -55,6 +56,7 @@ object PekkoDependency {
   val default = pekkoDependency(defaultVersion = minimumExpectedPekkoVersion)
   def docs = default
 
+  lazy val snapshot10x = Artifact(determineLatestSnapshot("1.0", false), true)
   lazy val mainSnapshot = Artifact(determineLatestSnapshot(), true)
 
   val pekkoVersion: String = default match {
@@ -93,21 +95,22 @@ object PekkoDependency {
       }
   }
 
-  private def determineLatestSnapshot(prefix: String = ""): String = {
+  private def determineLatestSnapshot(prefix: String = "", allowRCVersions: Boolean = true): String = {
     import sbt.librarymanagement.Http.http
     import gigahorse.GigahorseSupport.url
     import scala.concurrent.Await
     import scala.concurrent.duration._
 
-    val snapshotVersionR = """href=".*/((\d+)\.(\d+)\.(\d+)(-(M|RC)(\d+))?\+(\d+)-[0-9a-f]+-SNAPSHOT)/"""".r
+    lazy val snapshotRCVersionR = """href=".*/((\d+)\.(\d+)\.(\d+)(-(M|RC)(\d+))?\+(\d+)-[0-9a-f]+-SNAPSHOT)/"""".r
+    lazy val snapshotVersionR = """href=".*/((\d+)\.(\d+)\.(\d+)\+(\d+)-[0-9a-f]+-SNAPSHOT)/"""".r
 
     // pekko-cluster-sharding-typed_2.13 seems to be the last nightly published by `pekko-publish-nightly` so if that's there then it's likely the rest also made it
     val body = Await.result(http.run(url(
         s"${Resolver.ApacheMavenSnapshotsRepo.root}org/apache/pekko/pekko-cluster-sharding-typed_2.13/")),
       10.seconds).bodyAsString
 
-    val allVersions =
-      snapshotVersionR.findAllMatchIn(body)
+    val allVersions = if (allowRCVersions) {
+      snapshotRCVersionR.findAllMatchIn(body)
         .map {
           case Groups(full, ep, maj, min, _, _, tagNumber, offset) =>
             (
@@ -117,8 +120,21 @@ object PekkoDependency {
               Option(tagNumber).map(_.toInt),
               offset.toInt) -> full
         }
-        .filter(_._2.startsWith(prefix))
-        .toVector.sortBy(_._1)
-    allVersions.last._2
+    } else {
+      snapshotVersionR.findAllMatchIn(body)
+        .map {
+          case Groups(full, ep, maj, min, offset) =>
+            (
+              ep.toInt,
+              maj.toInt,
+              min.toInt,
+              None,
+              offset.toInt) -> full
+        }
+    }
+    allVersions
+      .filter(_._2.startsWith(prefix))
+      .toVector.sortBy(_._1)
+      .last._2
   }
 }
