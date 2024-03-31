40d82ff377d93a45887cb93922e5aa5dc02acb4b
Cedric Vandendriessche <cedric.vddriessche@telenet.be>
2024-03-31 21:47:48 +0200

    Improve too large chunk size test
    The current test goes over the configured max chunk size when parsing
    the last hex char. The spirit of the code is to continue parsing the
    size even if we've already gone over the max chunk size. To actually
    test this we need to make sure another char follows after we've gone
    over the max chunk size.

--- http-core/src/test/scala/org/apache/pekko/http/impl/engine/parsing/RequestParserSpec.scala
+++ http-core/src/test/scala/org/apache/pekko/http/impl/engine/parsing/RequestParserSpec.scala
@@ -544,11 +544,11 @@ abstract class RequestParserSpec(mode: String, newLine: String) extends AnyFreeS
       "too-large chunk size" in new Test {
         Seq(
           start,
-          """400000
+          """1400000
             |""") should generalMultiParseTo(
           Right(baseRequest),
           Left(
-            EntityStreamError(ErrorInfo("HTTP chunk of 4194304 bytes exceeds the configured limit of 1048576 bytes"))))
+            EntityStreamError(ErrorInfo("HTTP chunk of 20971520 bytes exceeds the configured limit of 1048576 bytes"))))
         closeAfterResponseCompletion shouldEqual Seq(false)
       }
 
