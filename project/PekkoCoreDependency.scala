4481c8432062cfb440cff384a06f290afdfd360f
PJ Fanning <pjfanning@users.noreply.github.com>
2025-08-26 15:37:50 +0100

    Update validate-and-test.yml
    updates
    
    more changes
    
    remove Java 8 specific code
    
    Update StringTools.scala
    
    use Java 17 in build
    
    snapshot dependency
    
    start to remove use of PekkoSSLConfig
    
    update
    
    Update ConnectionContext.scala
    
    Update remove-deprecated-methods.excludes
    
    try SNI matcher
    
    Update ClientServerSpec.scala
    
    try to fix test
    
    remove bindAndHandleAsync
    
    remove use of deprecated TLS methods
    
    begin removing deprecations
    
    Update HttpChallenge.java
    
    Update CorsSettings.scala
    
    encoder
    
    Update CookieDirectivesExamplesTest.java
    
    Update SizeLimitSpec.scala
    
    compile issues
    
    Update Encoder.scala
    
    mima
    
    Update Encoder.scala
    
    Update Encoder.scala
    
    more changes
    
    issues
    
    Update SizeLimitSpec.scala
    
    rename file
    
    more
    
    more
    
    Update HeaderDirectivesSpec.scala
    
    Update remove-deprecated-methods.excludes
    
    more
    
    move mima
    
    unused imports
    
    Update ServerSettingsImpl.scala
    
    Update ServerSettings.scala
    
    start to remove use of PekkoSSLConfig
    
    update
    
    Update ConnectionContext.scala
    
    Update remove-deprecated-methods.excludes
    
    try SNI matcher
    
    Update ClientServerSpec.scala
    
    try to fix test
    
    Update remove-deprecated-methods.excludes
    
    remove use of deprecated TLS methods
    
    Update remove-deprecated-methods.excludes
    
    HttpChallenge
    
    more deprecations
    
    javafmt
    
    Delete remove-deprecated-methods.excludes

--- project/PekkoCoreDependency.scala
+++ project/PekkoCoreDependency.scala
@@ -20,5 +20,5 @@ import com.github.pjfanning.pekkobuild.PekkoDependency
 object PekkoCoreDependency extends PekkoDependency {
   override val checkProject: String = "pekko-cluster-sharding-typed"
   override val module: Option[String] = None
-  override val currentVersion: String = "1.3.0"
+  override val currentVersion: String = "2.0.0-M1"
 }
