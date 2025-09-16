1be6eac81affb2cdd716427addbfd162cc7384f9
PJ Fanning <pjfanning@users.noreply.github.com>
2025-09-16 03:50:01 +0100

    allow java durations to be used in pekko-http javadsl

--- http/src/main/scala/org/apache/pekko/http/javadsl/server/directives/TimeoutDirectives.scala
+++ http/src/main/scala/org/apache/pekko/http/javadsl/server/directives/TimeoutDirectives.scala
@@ -13,6 +13,7 @@
 
 package org.apache.pekko.http.javadsl.server.directives
 
+import java.time.{ Duration => JDuration }
 import java.util.function.{ Function => JFunction, Supplier }
 
 import scala.concurrent.duration.Duration
@@ -21,11 +22,13 @@ import org.apache.pekko
 import pekko.http.javadsl.model.{ HttpRequest, HttpResponse }
 import pekko.http.javadsl.server.Route
 import pekko.http.scaladsl.server.{ Directives => D }
+import pekko.util.JavaDurationConverters._
 
 import pekko.http.impl.util.JavaMapping.Implicits._
 
 abstract class TimeoutDirectives extends WebSocketDirectives {
 
+  // TODO find a way to support Java Duration here. Type Erasure on the Function causes issues
   def extractRequestTimeout(inner: JFunction[Duration, Route]): RouteAdapter = RouteAdapter {
     D.extractRequestTimeout { timeout => inner.apply(timeout).delegate }
   }
@@ -35,7 +38,10 @@ abstract class TimeoutDirectives extends WebSocketDirectives {
    *
    * Due to the inherent raciness it is not guaranteed that the update will be applied before
    * the previously set timeout has expired!
+   * @deprecated As of 1.3.0, use the overload taking a `java.time.Duration` instead.
    */
+  @Deprecated
+  @deprecated("use the overload taking a `java.time.Duration` instead.", "1.3.0")
   def withRequestTimeout(timeout: scala.concurrent.duration.Duration, inner: Supplier[Route]): RouteAdapter =
     RouteAdapter {
       D.withRequestTimeout(timeout) { inner.get.delegate }
@@ -46,11 +52,39 @@ abstract class TimeoutDirectives extends WebSocketDirectives {
    *
    * Due to the inherent raciness it is not guaranteed that the update will be applied before
    * the previously set timeout has expired!
+   * @since 1.3.0
    */
+  def withRequestTimeout(timeout: JDuration, inner: Supplier[Route]): RouteAdapter =
+    RouteAdapter {
+      D.withRequestTimeout(timeout.asScala) { inner.get.delegate }
+    }
+
+  /**
+   * Tries to set a new request timeout and handler (if provided) at the same time.
+   *
+   * Due to the inherent raciness it is not guaranteed that the update will be applied before
+   * the previously set timeout has expired!
+   * @deprecated As of 1.3.0, use the overload taking a `java.time.Duration` instead.
+   */
+  @Deprecated
+  @deprecated("use the overload taking a `java.time.Duration` instead.", "1.3.0")
   def withRequestTimeout(timeout: scala.concurrent.duration.Duration,
       timeoutHandler: JFunction[HttpRequest, HttpResponse],
       inner: Supplier[Route]): RouteAdapter = RouteAdapter {
     D.withRequestTimeout(timeout, in => timeoutHandler(in.asJava).asScala) { inner.get.delegate }
+  }
+
+  /**
+   * Tries to set a new request timeout and handler (if provided) at the same time.
+   *
+   * Due to the inherent raciness it is not guaranteed that the update will be applied before
+   * the previously set timeout has expired!
+   * @since 1.3.0
+   */
+  def withRequestTimeout(timeout: JDuration,
+      timeoutHandler: JFunction[HttpRequest, HttpResponse],
+      inner: Supplier[Route]): RouteAdapter = RouteAdapter {
+    D.withRequestTimeout(timeout.asScala, in => timeoutHandler(in.asJava).asScala) { inner.get.delegate }
   }
 
   def withoutRequestTimeout(inner: Supplier[Route]): RouteAdapter = RouteAdapter {
