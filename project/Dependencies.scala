349310895706f3366750a88e7fb86ad19f88fb69
PJ Fanning <pjfanning@users.noreply.github.com>
2025-12-11 15:21:38 +0100

    use latest scala releases (#893)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,9 +33,9 @@ object Dependencies {
 
   val scalafixVersion = _root_.scalafix.sbt.BuildInfo.scalafixVersion // grab from plugin
 
-  val scala212Version = "2.12.20"
-  val scala213Version = "2.13.17"
-  val scala3Version = "3.3.6"
+  val scala212Version = "2.12.21"
+  val scala213Version = "2.13.18"
+  val scala3Version = "3.3.7"
   val allScalaVersions = Seq(scala213Version, scala212Version, scala3Version)
 
   val Versions = Seq(
