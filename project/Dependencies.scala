fd11ea3e085c3d3c414460875d0c47f27567a9ea
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-07-08 18:20:53 +0200

    Update jackson-databind to 2.22.1 (#1145)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ import scala.language.implicitConversions
 object Dependencies {
   import DependencyHelpers._
 
-  val jacksonDatabindVersion = "2.22.0"
+  val jacksonDatabindVersion = "2.22.1"
   val jacksonDatabind3Version = "3.2.0"
   val jacksonXmlVersion = jacksonDatabindVersion
   val junit4Version = "4.13.2"
