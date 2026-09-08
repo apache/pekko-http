9018f18d7d5c53bfdbe9d67cda2481c4aff0c95b
Scala Steward <scala_steward@virtuslab.com>
2026-09-08 14:50:44 +0000

    Update scala-xml to 2.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -54,7 +54,7 @@ object Dependencies {
   }
 
   object Compile {
-    val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.4.0"
+    val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.5.0"
 
     // For pekko-http spray-json support
     val sprayJson = "io.spray" %% "spray-json" % "1.3.6"
