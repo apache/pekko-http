84a106198654e98091762d2f0e149a39a0ab8ce8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-22 16:13:26 +0200

    Update caffeine to 3.3.0 (#1311)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -65,7 +65,7 @@ object Dependencies {
     // For pekko-http-jackson3 support
     val jacksonDatabind3 = "tools.jackson.core" % "jackson-databind" % jacksonDatabind3Version
 
-    val caffeine = "com.github.ben-manes.caffeine" % "caffeine" % "3.2.4"
+    val caffeine = "com.github.ben-manes.caffeine" % "caffeine" % "3.3.0"
 
     val scalafix = ("ch.epfl.scala" %% "scalafix-core" % Dependencies.scalafixVersion).cross(CrossVersion.for3Use2_13)
 
